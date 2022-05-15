1360d95e170d25c472007472c4f4403524d919bf
choi <cjh5320@naver.com>
2022-05-15 22:56:57 +0900

    forth

--- main.js
+++ main.js
@@ -78,7 +78,7 @@ function play(){
         if(userValue=computerNum){
             resultArea.textContent = "맞춤"
         }else{gameOver=true
-            restchance.textContent = "실패"}
+            restchance.textContent = `실패 정답은 ${computerNum}이었습니다.`}
         
     }
     if(gameOver==true){
